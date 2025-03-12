7f7cff5386616fc67c5c6a2fd1c16d0d2abdaa79
Guillaume <guillaume.gobert@gmail.com>
2025-03-12 09:51:50 +0100

    ajout du style

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 /* Script pour la gestion du menu mobile */
-document.getElementById('burger_menu').addEventListener('click', function() {
+document.getElementById('menu-mobile').addEventListener('click', function() {
     document.getElementById('menu').classList.toggle('active');
 });
